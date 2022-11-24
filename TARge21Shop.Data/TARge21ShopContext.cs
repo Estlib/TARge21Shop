89e8e4ff96877beb59f186dba8f09ad979f5815f
jürivaitmaa <jyrivaitmaa@gmail.com>
2022-11-24 09:46:47 +0200

    paus

--- TARge21Shop.Data/TARge21ShopContext.cs
+++ TARge21Shop.Data/TARge21ShopContext.cs
@@ -4,10 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TARge21Shop.Core.Domain.Spaceship;
 
 namespace TARge21Shop.Data
 {
     public class TARge21ShopContext : DbContext
     {
+        public TARge21ShopContext(DbContextOptions<TARge21ShopContext> options) : base(options) { }
+
+        public DbSet<Spaceship> Spaceships { get; set; }
     }
 }
