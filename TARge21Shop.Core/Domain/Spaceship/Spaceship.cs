5daed602c1dfdbd9b1afb5f834d7179aaaab72f9
jürivaitmaa <jyrivaitmaa@gmail.com>
2022-11-24 09:34:01 +0200

    first commit

---
+++ TARge21Shop.Core/Domain/Spaceship/Spaceship.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARge21Shop.Core.Domain.Spaceship
+{
+    public class Spaceship
+    {
+        [Key]
+        public Guid? Id { get; set; }
+        public string? Name { get; set; }
+        public string? ShipType { get; set; }
+        public int CrewCount { get; set; }
+        public int PassangerCount { get; set; }
+        public int Cargo { get; set; }
+        public int FullTripCount { get; set; }  
+        public int MaintenanceCount { get; set; }
+        public DateTime LastMaintenance { get; set; } 
+        public DateTime BuildDate { get; set; }        
+        public int EnginePower { get; set; }
+        public DateTime MaidenLaunch { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+
+    }
+}
