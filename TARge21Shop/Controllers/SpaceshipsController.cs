92febafabf12ac0541effc61c64d0cbd3794b6de
jürivaitmaa <jyrivaitmaa@gmail.com>
2022-12-01 11:11:20 +0200

    index viewmodel

---
+++ TARge21Shop/Controllers/SpaceshipsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TARge21Shop.Data;
+using TARge21Shop.Models.Spaceship;
+
+
+namespace TARge21Shop.Controllers
+{
+    public class SpaceshipsController : Controller
+    {
+        private readonly TARge21ShopContext _context;
+
+        public SpaceshipsController(TARge21ShopContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var result = _context.Spaceships
+                .OrderByDescending(y => y.CreatedAt)
+                .Select(x => new SpaceshipIndexViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    ShipType = x.ShipType,
+                    PassangerCount = x.PassangerCount,
+                    CrewCount = x.CrewCount,
+                    Cargo = x.Cargo,
+                    EnginePower = x.EnginePower
+                });
+
+            return View(result);
+        }
+    }
+}
